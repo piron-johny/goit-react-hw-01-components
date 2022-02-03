1be72a5e884e21fd3031a07a0855a5389e59ef27
Piron Evgeniy <piron.johny@gmail.com>
2022-02-03 07:48:47 +0000

    подключил React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from './components/Profile/Profile';
+
+import './App.css';
+
+function App() {
+  return (
+    <Profile />
+  )
+}
+
+export default App;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile(props) {
+  return <Profile />
+}
+
+Profile.propsType = {
+  username: propsType.string,
+  tag: propsType.string,
+  location: propsType.string,
+  avatar: propsType.string,
+  stats: propsType.string,
+}
+export default Profile
